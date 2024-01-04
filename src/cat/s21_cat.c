281dde8b1754202251aebdc430754815e4d6665a
unknown <huldades@student.21-school.ru>
2024-01-04 22:53:50 +0300

    Pre test

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -1,28 +1,38 @@
+// TODO: парсинг параметров после файла, makefile путь exe в билд и убрать ехе
 #include "s21_cat.h"
 
-int main(int argcount, char *argument[]) {
+int main(int arg_count, char *argument[]) {
   flags flag = {0};
+  int wrong_flag = 0;
 
-  for (int i = 1; i < argcount; i++) {
-    if (argument[i][0] == '-') {
-      flag = get_flag(argument[i], flag);
-    } else {
-      print_f(argument[i], flag);
-    }
+  for (int i = 1; i < arg_count && !wrong_flag; i++) {
+    if (argument[i][0] == '-') flag = get_flag(argument[i], flag, &wrong_flag);
+  }
+
+  for (int i = 1; i < arg_count && !wrong_flag; i++) {
+    if (argument[i][0] != '-') print_f(argument[i], flag);
   }
 
   return 0;
 }
 
-flags get_flag(char c[], flags flag) {
+flags get_flag(char arg[], flags flag, int *wrong_flag) {
   int i = 0;
-  if (c[1] == '-') {
-    if (strcmp(c, "--number-nonblank") == 0) flag.b = 1;
-    if (strcmp(c, "--number") == 0) flag.n = 1;
-    if (strcmp(c, "--squeeze-blank") == 0) flag.s = 1;
+  if (arg[1] == '-') {
+    if (strcmp(arg, "--number-nonblank") == 0)
+      flag.b = 1;
+    else if (strcmp(arg, "--number") == 0)
+      flag.n = 1;
+    else if (strcmp(arg, "--squeeze-blank") == 0)
+      flag.s = 1;
+    else {
+      fprintf(stderr, "cat: %s: Unknown option\n", arg);
+      *wrong_flag = 1;
+    }
+
   } else
-    while (c[++i] != '\0') {
-      switch (c[i]) {
+    while (arg[++i] != '\0' && !*wrong_flag) {
+      switch (arg[i]) {
         case 'b':
           flag.b = 1;
           break;
@@ -46,8 +56,13 @@ flags get_flag(char c[], flags flag) {
         case 'T':
           flag.t = 1;
           break;
+        case 'v':
+          flag.v = 1;
+          break;
         default:
-          fprintf(stderr, "cat: -%c: Unknown option\n", c[i]);
+          fprintf(stderr, "cat: -%c: Unknown option\n",
+                  arg[i]);  // потестить эту ошибку с большим колвом аргументов
+          *wrong_flag = 1;
           break;
       }
     }
@@ -57,39 +72,49 @@ flags get_flag(char c[], flags flag) {
 }
 
 void print_f(char *fil, flags flag) {
-  // printf("b = %d, e = %d, n = %d, s = %d, t = %d, v = %d\n", flag.b, flag.e,
-  //        flag.n, flag.s, flag.t, flag.v);
   FILE *f = fopen(fil, "r");
   if (f == NULL) {
     fprintf(stderr, "cat: %s: No such file or directory\n", fil);
   }
 
-  int ch0 = ' ', ch1 = '\n', ch2, str_count = 0;
+  static int ch0 = ' ', ch1 = '\n', ch2;
 
   while ((ch2 = fgetc(f)) != EOF) {
-    if (flag.s == 0 || ch0 != '\n' || ch1 != '\n' || ch2 != '\n') {
-      if (flag.b && ch2 != '\n' && ch1 == '\n') printf("%6d\t", ++str_count);
-
-      if (flag.n && ch1 == '\n') printf("%6d\t", ++str_count);
-
-      if (flag.e && ch2 == '\n') printf("$");
-
-      if (flag.t && ch2 == '\t') {
-        printf("^");
-        ch2 += 64;
-      }
-
-      if (flag.v && ((ch2 < 32 && ch2 != '\n' && ch2 != '\t') || ch2 == 127)) {
-        printf("^");
-        ch2 = ch2 == 127 ? ch2 - 128 + 64 : ch2 + 64;
-      }
-
-      printf("%c", ch2);
-    }
-
+    print_ch(ch0, ch1, ch2, flag);
     ch0 = ch1;
     ch1 = ch2;
   }
 
   fclose(f);
+}
+
+void print_ch(int ch0, int ch1, int ch2, flags flag) {
+  if (flag.s == 0 || ch0 != '\n' || ch1 != '\n' || ch2 != '\n') {
+    static int str_count = 0;
+
+    if (flag.b && ch2 != '\n' && ch1 == '\n') printf("%6d\t", ++str_count);
+
+    if (flag.n && ch1 == '\n') printf("%6d\t", ++str_count);
+
+    if (flag.e && ch2 == '\n') printf("$");
+
+    if (flag.t && ch2 == '\t') {
+      printf("^");
+      ch2 += 64;
+    }
+
+    if (flag.v && ((ch2 < 32 && ch2 != '\n' && ch2 != '\t') || ch2 >= 127)) {
+      if (ch2 > 127) {  // METACHAR
+        printf("M-");
+        ch2 -= 128;
+      }
+
+      if (ch2 < 32 || ch2 == 127) {
+        printf("^");
+        ch2 = ((ch2 == 127) ? ch2 - 128 + 64 : ch2 + 64);
+      }
+    }
+
+    printf("%c", ch2);
+  }
 }
--- src/cat/s21_cat.h
+++ src/cat/s21_cat.h
@@ -6,5 +6,6 @@ typedef struct flags {
   int b, e, n, s, t, v;
 } flags;
 
-flags get_flag(char c[], flags flag);
+flags get_flag(char c[], flags flag, int *wrong_flag);
 void print_f(char *fil, flags flag);
+void print_ch(int ch0, int ch1, int ch2, flags flag);
